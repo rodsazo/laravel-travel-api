8e0073025cb5820187f890cbebaeb2e8e1fee84c
Rodrigo Murillo <rmurillo@creedinteractive.com>
2023-12-14 23:04:41 -0500

    add cviebrock/eloquent-sluggable package

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -22,9 +22,7 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
-    protected $observers = [
-        Travel::class => TravelObserver::class
-    ];
+    protected $observers = [];
 
     /**
      * Register any events for your application.
