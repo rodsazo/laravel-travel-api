1d70031306aed76df583ec648a3df32fcaffe7a0
Rodrigo Murillo <rmurillo@creedinteractive.com>
2023-12-14 23:11:16 -0500

    feature (Travel model): add number of nights attribute

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -27,5 +28,12 @@ class Travel extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    public function numberOfNights() : Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
     }
 }
