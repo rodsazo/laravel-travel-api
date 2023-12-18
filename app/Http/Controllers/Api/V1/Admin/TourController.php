b4d746cefd460f4aeb583194145ca5fe8aae3f86
Rodrigo Murillo <rmurillo@creedinteractive.com>
2023-12-18 16:58:54 -0500

    chore (docs): Add documentation for private endpoints

--- app/Http/Controllers/Api/V1/Admin/TourController.php
+++ app/Http/Controllers/Api/V1/Admin/TourController.php
@@ -7,8 +7,25 @@ use App\Http\Requests\TourRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
 
+/**
+ * @group Admin endpoints
+ */
 class TourController extends Controller
 {
+    /**
+     * POST Tour
+     *
+     * Creates a new tour record.
+     *
+     * @authenticated
+     *
+     * @bodyParam name number. Example: "A night-tour of Cuzco"
+     * @bodyParam starting_date date. Example: "2023-12-10"
+     * @bodyParam ending_date date. Example: "2023-12-20"
+     * @bodyParam price number. Example: "123.25"
+     *
+     * @response {"data":{"id":"996a36ca-2693-4901-9c55-7136e68d81d5","name":"My new tour","starting_date":"2023-12-15", ...}
+     */
     public function store(Travel $travel, TourRequest $request)
     {
         $tour = $travel->tours()->create($request->validated());
--- app/Http/Controllers/Api/V1/Admin/TravelController.php
+++ app/Http/Controllers/Api/V1/Admin/TravelController.php
@@ -7,8 +7,27 @@ use App\Http\Requests\TravelRequest;
 use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 
+/**
+ * @group Admin endpoints
+ */
 class TravelController extends Controller
 {
+
+    /**
+     * POST Travel
+     *
+     * Creates a new Travel record.
+     *
+     * @authenticated
+     *
+     * @bodyParam is_public boolean. Example: "1"
+     * @bodyParam name string. Example: "My new travel"
+     * @bodyParam description string. Example: "a 5-days 4-nights city tour of Cuzco"
+     * @bodyParam number_of_days number. Example: "5"
+     *
+     * @response {"data":{"id":"996a36ca-2693-4901-9c55-7136e68d81d5","name":"My new travel 234","slug":"my-new-travel-234", ...}
+     * @response 422 {"message":"The name has already been taken.","errors":{"name":["The name has already been taken."]}}
+     */
     public function store(TravelRequest $request)
     {
         $travel = Travel::create($request->validated());
@@ -16,6 +35,21 @@ class TravelController extends Controller
         return new TravelResource($travel);
     }
 
+    /**
+     * PUT Travel
+     *
+     * Updates new Travel record.
+     *
+     * @authenticated
+     *
+     * @bodyParam is_public boolean.
+     * @bodyParam name string. Example: "My new travel"
+     * @bodyParam description string. Example: "a 5-days 4-nights city tour of Cuzco"
+     * @bodyParam number_of_days number. Example: "5"
+     *
+     * @response {"data":{"id":"996a36ca-2693-4901-9c55-7136e68d81d5","name":"My new travel 234", ...}
+     * @response 422 {"message":"The name has already been taken.","errors":{"name":["The name has already been taken."]}}
+     */
     public function update(Travel $travel, TravelRequest $request)
     {
         $travel->update($request->validated());
