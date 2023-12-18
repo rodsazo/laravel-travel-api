0388b84128f6ae47b48057418c42a74c4f4dcfdc
Rodrigo Murillo <rmurillo@creedinteractive.com>
2023-12-17 19:57:37 -0500

    feature (private endpoints): add login endpoint

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -3,15 +3,35 @@
 namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
-        //
+        // We get the User from the DB by their email
+        $the_user = User::where('email', $request->email)->first();
+
+        // We checked the provided password against the hashed password in the DB
+
+        if( !$the_user || ! Hash::check($request->password, $the_user->password )){
+            // If the passwords don't match, we return the 422 status code with a validation error
+            return response()->json([
+                'error' => 'The provided credentials are incorrect'
+            ], 422);
+        }
+
+        $device = substr( $request->userAgent() ?? '', 0, 255 );
+        // If passwords are ok, we generate and return a personal access token with Laravel Sanctum
+
+        return response()->json([
+            'access_token' => $the_user->createToken( $device )->plainTextToken
+        ]);
     }
 }
