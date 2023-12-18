538af7ac01ad3b6b4fc2989470940f737774bc3a
Rodrigo Murillo <rmurillo@creedinteractive.com>
2023-12-18 16:45:22 -0500

    chore (docs): Add scribe and documentation for public endpoints

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -7,8 +7,32 @@ use App\Http\Requests\TourListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
 
+/**
+ * @group Public endpoints
+ */
 class TourController extends Controller
 {
+
+    /**
+     * GET Travel Tours
+     *
+     * Returns the paginated list of tours by travel slug.
+     *
+     * @urlParam travel_slug string Travel slug. Example: first-travel
+     *
+     * @bodyParam priceFrom number. Example: "144.99"
+     * @bodyParam priceTo number. Example: "200.30"
+     * @bodyParam dateFrom date. Example: "2023-12-15"
+     * @bodyParam dateTo date. Example: "2023-12-31"
+     * @bodyParam sortBy string. Example: "price"
+     * @bodyParam sortOrder string. Example: "asc" or "desc"
+     *
+     * @response {"data":[{"id":"9958e389-5edf-48eb-8ecd-e058985cf3ce","name":"Tour on Sunday","starting_date":"2023-06-11","ending_date":"2023-06-16", ...}
+     *
+     * @param Travel $travel
+     * @param TourListRequest $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index(Travel $travel, TourListRequest $request)
     {
 
